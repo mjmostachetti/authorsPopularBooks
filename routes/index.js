95a97187c591b4874b080724027de677addd2b94
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-12 16:37:38 -0700

    Starting to workout the goodreads api calls

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('findAuthor', { title: 'Express' });
 });
+
+//api call to return an authors books
+router.get('/findAuthorsBooks', function(request,response){
+  console.log(request.body)
+  /*
+  https('https://www.goodreads.com/api/author_url/' + 
+    Orson%20Scott%20Card?key=qjXRyTtjvpFSAa8N8VL8Iw')
+  */
+})
 
 module.exports = router;
