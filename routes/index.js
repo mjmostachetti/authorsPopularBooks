c32b64a11046b9e59ee6cbbe80f531456cf09601
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-17 02:44:43 -0700

    Learned about converting XML to JSON via xml2js npm module. Getting closer to the final api call.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https')
+var xml2js = require('xml2js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,12 +15,25 @@ router.get('/partials/:name', function (req, res){
 
 //api call to return an authors books
 router.get('/findAuthorsBooks/:authorName', function(request,response){
-  console.log(request.body)
-  console.log(request.params.authorName)
-  /*
-  https('https://www.goodreads.com/api/author_url/' + 
-    Orson%20Scott%20Card?key=qjXRyTtjvpFSAa8N8VL8Iw')
-  */
+  var authorsName = request.params.authorName
+  console.log(authorsName)
+  var authorXML = '';
+  var parseString = xml2js.parseString
+  https.get('https://www.goodreads.com/api/author_url/' + 
+    authorsName + '?key=qjXRyTtjvpFSAa8N8VL8Iw',
+    function(res){
+
+      res.on('data', function(d){
+        authorXML += d;
+      })
+
+      res.on('end', function(){
+        console.log(authorXML)
+        parseString(authorXML, function (err, result) {
+            console.log(result.GoodreadsResponse.author);
+        });
+      })
+    })
 })
 
 module.exports = router;
