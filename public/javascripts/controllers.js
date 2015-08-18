d3e3a175ca85598b19a0d3f42f1826310346c0c1
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-17 17:14:09 -0700

    Added scaling to author data

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -7,15 +7,33 @@ bookControllers.controller('BookListCtrl', ['$scope','$http',
     });
     $scope.orderProp = 'age';
     $scope.search = function(){
+      $('#svgDocker').empty();
       console.log($('#searchAuthor').val())
       var authorsName = $('#searchAuthor').val()
       $http.get('findAuthorsBooks/' + authorsName)
       .success(function(data){
         $scope.authorObj = data;
+        console.log($scope.authorObj.books[0].book)
+
+        var linearScale = d3.scale.linear()
+          .domain([0,5])
+          .range([0,500])
         var svgContainer = d3.select('#svgDocker').append('svg')
-          .attr('width',400)
-          .attr('height',400)
-          
+          .attr('width',500)
+          .attr('height',500)
+        var circles = svgContainer.selectAll('circle')
+          .data($scope.authorObj.books[0].book)
+          .enter()
+          .append('circle')
+        var circleAttributes = circles
+          .attr('cx',function(d){ 
+            return linearScale(parseFloat(d.average_rating[0],10));
+          })
+          .attr('cy',function(d){ 
+            return linearScale(parseFloat(d.average_rating[0],10));
+          })
+          .attr('r',5)
+          .text(function(d){return d.title[0]})
       })
     };
 
