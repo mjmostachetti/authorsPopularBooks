c28b1b18e96e48229dfa509e285f575db6fdf9f4
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-17 22:35:39 -0700

    First working iteration of visualization

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -7,35 +7,98 @@ bookControllers.controller('BookListCtrl', ['$scope','$http',
     });
     $scope.orderProp = 'age';
     $scope.search = function(){
-      $('#svgDocker').empty();
       console.log($('#searchAuthor').val())
       var authorsName = $('#searchAuthor').val()
       $http.get('findAuthorsBooks/' + authorsName)
       .success(function(data){
         $scope.authorObj = data;
-        console.log($scope.authorObj.books[0].book)
+        var allBooks = $scope.authorObj.books[0].book
+        var tenBooks = allBooks.slice(0,10)
+        var margin = {top: 80, right: 180, bottom: 80, left: 180},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
-        var linearScale = d3.scale.linear()
-          .domain([0,5])
-          .range([0,500])
-        var svgContainer = d3.select('#svgDocker').append('svg')
-          .attr('width',500)
-          .attr('height',500)
-        var circles = svgContainer.selectAll('circle')
-          .data($scope.authorObj.books[0].book)
-          .enter()
-          .append('circle')
-        var circleAttributes = circles
-          .attr('cx',function(d){ 
-            return linearScale(parseFloat(d.average_rating[0],10));
-          })
-          .attr('cy',function(d){ 
-            return linearScale(parseFloat(d.average_rating[0],10));
-          })
-          .attr('r',5)
-          .text(function(d){return d.title[0]})
-      })
-    };
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1, .3);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(8, "%");
+        $('#svgDocker').empty();
+        var svg = d3.select("#svgDocker").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(tenBooks.map(function(d) { return d.title[0]; }));
+        y.domain([0, d3.max(tenBooks, function(d) { return d.average_rating[0]; })]);
+
+        svg.append("text")
+            .attr("class", "title")
+            .attr("x", x(tenBooks[0].title[0]))
+            .attr("y", -26)
+            .text("Why Are We Leaving Facebook?");
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+          .selectAll(".tick text")
+            .call(wrap, x.rangeBand());
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        svg.selectAll(".bar")
+            .data(tenBooks)
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.title[0]); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.average_rating[0]); })
+            .attr("height", function(d) { return height - y(d.average_rating[0]); });
+
+        function wrap(text, width) {
+          text.each(function() {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+            while (word = words.pop()) {
+              line.push(word);
+              tspan.text(line.join(" "));
+              if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+              }
+            }
+          });
+        }
+
+        function type(d) {
+          d.value = +d.value;
+          return d;
+        }
+    })
+  };
+
 
 }]);
 
@@ -58,3 +121,27 @@ bookControllers.controller('AbeBooksCtrl',
       })
   }
 ]);
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
